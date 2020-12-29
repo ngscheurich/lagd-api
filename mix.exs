8cd5aa0b7084b6a477ac47f025ee44af52d63244
Nicholas Scheurich <nick@scheurich.me>
2020-12-29 10:44:15 -0600

    Update language versions

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule LAGD.MixProject do
     [
       app: :lagd,
       version: File.read!("VERSION"),
-      elixir: "~> 1.5",
+      elixir: "~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
